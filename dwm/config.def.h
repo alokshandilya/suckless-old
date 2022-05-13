78dc4505d320b236616d0cbfe74c575e76ff5965
Alok Shandilya <alok.shandilya10@gmail.com>
2022-05-13 13:39:39 +0530

    bottom bar dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -19,7 +19,7 @@ static const unsigned int systrayspacing 	= 2;   		// systray spacing
 static const int systraypinningfailfirst 	= 1;   		// 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor
 static const int showsystray        		= 1;     	// 0 means no systray
 static const int showbar            		= 1;        // 0 means no bar
-static const int topbar             		= 1;        // 0 means bottom bar
+static const int topbar             		= 0;        // 0 means bottom bar
 
 static const char *fonts[]     		= { "JetBrainsMono Nerd Font:style:medium:size=11",
                                   		"JoyPixels:size=12:antialias=true:autohint=true"};
