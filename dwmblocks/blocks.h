de82afff9307fb29bb27dd1ffb426061a65c20aa
Alok Shandilya <alok.shandilya10@gmail.com>
2022-05-14 13:26:45 +0530

    add colored emojis in dwmblocks

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,29 +1,30 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    //{" 🐧 ", "$HOME/.config/scripts/kernel",		    360,		        2},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    // {" 🐧 ", "$HOME/.config/scripts/kernel", 360, 2},
 
-	//{" ⛅ ", "/home/aloks/.config/scripts/weather",		60,				1},
+    // {" ⛅ ", "/home/aloks/.config/scripts/weather",	60, 1},
 
-	//{"  ", "$HOME/.config/scripts/upt",		        60,		            2},
-	{"🔺 ", "$HOME/.config/scripts/upt",		        60,		            2},
+    // {"  ", "$HOME/.config/scripts/upt", 60, 2},
+    {"🔺 ", "$HOME/.config/scripts/upt", 60, 2},
 
-	//{" 📦 ", "$HOME/.config/scripts/pacupdate",		360,		        9},
-	
-	//{" 💻 ", "$HOME/.config/scripts/memory",	        6,		            1},
-	{" ", "$HOME/.config/scripts/memory",	        6,		            1},
+    // {" 📦 ", "$HOME/.config/scripts/pacupdate",		360, 9},
 
-	//{" 🔋 ", "$HOME/.config/scripts/battery",			150,			1},
-	{"B ", "$HOME/.config/scripts/battery",			150,			1},
+    {" 💻 ", "$HOME/.config/scripts/memory", 6, 1},
+    // {" ", "$HOME/.config/scripts/memory", 6, 1},
 
-	//{" 🔊 ", "$HOME/.config/scripts/volume",			2,		            10},
-	{"V ", "$HOME/.config/scripts/volume",			2,		            10},
+    {" 🔋 ", "$HOME/.config/scripts/battery", 150, 1},
+    // {"B ", "$HOME/.config/scripts/battery", 150, 1},
 
+    {" 🔊 ", "$HOME/.config/scripts/volume", 2, 10},
+    // {"V ", "$HOME/.config/scripts/volume", 2, 10},
 
-	//{" � ", "$HOME/.config/scripts/clock",			5,		            0},
-	{" ", "$HOME/.config/scripts/clock",			5,		            0},
+    {" 🕑 ", "$HOME/.config/scripts/clock", 5, 0},
+    // {" ", "$HOME/.config/scripts/clock", 5, 0},
 
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
 static char delim = '|';
