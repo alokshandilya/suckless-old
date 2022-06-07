5469448620e19155fccfbd18271c97bb4d057f1c
Alok Shandilya <alok.shandilya10@gmail.com>
2022-06-08 02:11:39 +0530

    add cool-autostart patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -110,6 +110,20 @@ static const char *colors[][3] = {
 	[SchemeTitle]	= { col_titletext,	col_midbar,		col_cyan  },
 };
 
+static const char *const autostart[] = {
+    "sh", "-c", "/usr/bin/emacs --daemon", NULL,
+    "./.fehbg", NULL,
+    "sh", "-c", "picom --config ~/.config/picom/picom.conf --experimental-backends", NULL,
+    "lxsession", NULL,
+    "st", NULL,
+    "dunst", NULL,
+    "xbanish", NULL,
+    "numlockx", "on", NULL,
+    "sh", "-c", "conky -c ~/.config/conky/gruvbox-material.conkyrc", NULL,
+    "sh", "-c", "~/Documents/GitHub/suckless/dwmblocks/dwmblocks", NULL,
+    NULL /* terminate */
+};
+
 // tagging
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 // static const char *tags[] = { "", "", "", " ", "", "", "", "", "", "", "", "", "", "" };
