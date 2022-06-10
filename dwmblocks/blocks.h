8c83b4edd1a8f957ce7765aa34efb6ca1261192c
Alok Shandilya <alok.shandilya10@gmail.com>
2022-06-10 09:08:41 +0530

    initial commit

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,25 +4,26 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
     // ICONS [requires libxft-bgra]
-    {" 🔺 ", "$HOME/.config/scripts/upt", 60, 2},
-    {" 📦 ", "$HOME/.config/scripts/pacupdate", 300, 9},
-    {" ⛅ ", "/home/aloks/.config/scripts/weather", 60, 1},
-    // {" 🐧 ", "$HOME/.config/scripts/kernel", 360, 2},
-    {" 💻 ", "$HOME/.config/scripts/cpu", 2, 1},
-    {" 💾 ", "$HOME/.config/scripts/memory", 6, 1},
-    {" 🔋 ", "$HOME/.config/scripts/battery", 150, 1},
-    {" 🔊 ", "$HOME/.config/scripts/volume", 2, 10},
-    {" 🕑 ", "$HOME/.config/scripts/clock", 5, 0},
+    // {" 🔺 ", "$HOME/.config/scripts/upt", 60, 2},
+    // {" 📦 ", "$HOME/.config/scripts/pacupdate", 300, 9},
+    // {" ⛅ ", "/home/aloks/.config/scripts/weather", 60, 1},
+    // // {" 🐧 ", "$HOME/.config/scripts/kernel", 360, 2},
+    // {" 💻 ", "$HOME/.config/scripts/cpu", 2, 1},
+    // {" 💾 ", "$HOME/.config/scripts/memory", 6, 1},
+    // {" 🔋 ", "$HOME/.config/scripts/battery", 150, 1},
+    // {" 🔊 ", "$HOME/.config/scripts/volume", 2, 10},
+    // {" 🕑 ", "$HOME/.config/scripts/clock", 5, 0},
 
     // NERD FONTS :
-    // {" 卑  ", "$HOME/.config/scripts/upt", 60, 2},
-    // {"   ", "$HOME/.config/scripts/pacupdate", 360, 9},
-    // {"   ", "/home/aloks/.config/scripts/weather", 60, 1},
-    // {"  ", "$HOME/.config/scripts/kernel", 360, 2},
-    // {"   ", "$HOME/.config/scripts/memory", 6, 1},
-    // {"   ", "$HOME/.config/scripts/battery", 150, 1},
-    // {"  ", "$HOME/.config/scripts/volume", 2, 10},
-    // {"  ", "$HOME/.config/scripts/clock", 5, 0},
+    {" 卑  ", "$HOME/.config/scripts/upt", 60, 2},
+    {"   ", "$HOME/.config/scripts/pacupdate", 360, 9},
+    {"   ", "/home/aloks/.config/scripts/weather", 60, 1},
+    {"  ", "$HOME/.config/scripts/kernel", 360, 2},
+    {"  ", "$HOME/.config/scripts/cpu", 2, 1},
+    {"   ", "$HOME/.config/scripts/memory", 6, 1},
+    {"   ", "$HOME/.config/scripts/battery", 150, 1},
+    {"  ", "$HOME/.config/scripts/volume", 2, 10},
+    {"  ", "$HOME/.config/scripts/clock", 5, 0},
 
     // AWESOME FONTS :
     // {"  ", "$HOME/.config/scripts/upt", 60, 2},
