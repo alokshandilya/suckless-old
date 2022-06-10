9483e0e733e17e7873ddfec2e85954066ac5ab17
Alok Shandilya <alok.shandilya10@gmail.com>
2022-06-10 18:14:38 +0530

    status2d awesome fonts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -25,15 +25,15 @@ static const Block blocks[] = {
     // {"  ", "$HOME/.config/scripts/clock", 5, 0},
 
     // AWESOME FONTS :
-    // {"  ", "$HOME/.config/scripts/upt", 60, 2},
-    // {"   ", "$HOME/.config/scripts/pacupdate", 360, 9},
-    // {"   ", "/home/aloks/.config/scripts/weather", 60, 1},
-    // {"  ", "$HOME/.config/scripts/kernel", 360, 2},
-    // {"   ", "$HOME/.config/scripts/memory", 6, 1},
-    // {"   ", "$HOME/.config/scripts/battery", 150, 1},
-    // {"  ", "$HOME/.config/scripts/volume", 2, 10},
-    // {"  ", "$HOME/.config/scripts/clock", 5, 0},
-
+    // {"^C5^  ^d^", "$HOME/.config/scripts/upt", 60, 2},
+    // {"^C3^  ^d^", "$HOME/.config/scripts/pacupdate", 360, 9},
+    // {"^C4^  ^d^", "/home/aloks/.config/scripts/weather", 60, 1},
+    // // {"^C1^  ^d^", "$HOME/.config/scripts/kernel", 360, 2},
+    // {"^C6^  ^d^", "$HOME/.config/scripts/cpu", 2, 1},
+    // {"^C5^  ^d^", "$HOME/.config/scripts/memory", 6, 1},
+    // {"^C3^  ^d^", "$HOME/.config/scripts/battery", 150, 1},
+    // {"^C4^  ^d^", "$HOME/.config/scripts/volume", 2, 10},
+    // {"^C1^  ^d^", "$HOME/.config/scripts/clock", 5, 0}, 
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
